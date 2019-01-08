b2e2041c487bbeacfaab600b920b5c15dc6e033a
David Moncayo <>
2019-01-08 14:15:25 -0500

    Unreal project created

---
+++ BattleTak/Source/BattleTak/BattleTakGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTakGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETAK_API ABattleTakGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

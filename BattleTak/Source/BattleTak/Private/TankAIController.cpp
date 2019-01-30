9c9f6f3fb61342a69208857389b124ef6c9c3c7d
David Moncayo <>
2019-01-29 20:12:01 -0500

    Added TankPlayerController class Added TankAIController class Add Tank class

---
+++ BattleTak/Source/BattleTak/Private/TankAIController.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+#include "BattleTak.h"
+#include "TankPlayerController.h"
+
+void ATankAIController::BeginPlay()
+{
+    Super::BeginPlay();
+    
+    auto ControlledTank = GetControlledTank();
+    if (!ControlledTank)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("IAController not possessing a tank"));
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("IAController possesing: %s"), *(ControlledTank->GetName()));
+    }
+    
+    auto PlayerTank = GetPlayerTank();
+    if (!PlayerTank)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("IAController haven't found player's tank"));
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("IAController have found player's tank: %s"), *(PlayerTank->GetName()));
+    }
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+    return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+    auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
+    if (!PlayerTank) { return nullptr; }
+    
+    return Cast<ATank>(PlayerTank);
+    
+}
---
+++ BattleTak/Source/BattleTak/Private/TankPlayerController.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "TankPlayerController.h"
+#include "BattleTak.h"
+
+
+void ATankPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+    
+    auto ControlledTank = GetControlledTank();
+    
+    if (!ControlledTank)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("PlayController not possessing a tank"));
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("PlayController possesing: %s"), *(ControlledTank->GetName()));
+    }
+    
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+    return Cast<ATank>(GetPawn());
+}

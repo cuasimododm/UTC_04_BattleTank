793cb021c311926b6df6b35e9adcb5b2edbf2ad3
David Moncayo <>
2019-01-31 13:49:14 -0500

    Iterating over the Aim functionality

---
+++ BattleTak/Source/BattleTak/Private/TankAimingComponent.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAimingComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values for this component's properties
+UTankAimingComponent::UTankAimingComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent* BarrelToSet)
+{
+    Barrel = BarrelToSet;
+}
+
+
+void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
+{
+    if (!Barrel) { return; }
+    
+    FVector OutLaunchVelocity(0);
+    FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
+    
+    bool bHaveAimSolution = UGameplayStatics::SuggestProjectileVelocity(this,
+                                                                        OutLaunchVelocity,
+                                                                        StartLocation,
+                                                                        HitLocation,
+                                                                        LaunchSpeed,
+                                                                        ESuggestProjVelocityTraceOption::DoNotTrace);
+    
+    // Calculate the OutLaunchVelocity
+    if (bHaveAimSolution)
+    {
+        FVector AimDirection = OutLaunchVelocity.GetSafeNormal();
+        MoveBarrelTowards(AimDirection);
+        
+    }
+    // If no solution found do nothing
+    
+}
+
+void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
+{
+    // Work-out difference between current barrel rotation, and AimDirection
+    auto BarrelRotator = Barrel->GetForwardVector().Rotation();
+    auto AimAsRotator = AimDirection.Rotation();
+    auto DeltaRotator = AimAsRotator - BarrelRotator;
+    
+    // Move the barrel the right amount this frame
+    
+    // Given max eleveation speed, and the frame time
+}

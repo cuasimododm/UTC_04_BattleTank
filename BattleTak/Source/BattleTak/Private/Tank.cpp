11dde3764c12318ad140b04f4151dde77158402d
David Moncayo <>
2019-02-05 11:41:00 -0500

    Added function to Fire Added Projectile class and blueprint

--- BattleTak/Source/BattleTak/Private/Tank.cpp
+++ BattleTak/Source/BattleTak/Private/Tank.cpp
@@ -29,11 +29,6 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 }
 
-void ATank::AimAt(FVector HitLocation)
-{
-    TankAimingComponent->AimAt(HitLocation, LaunchSpeed);
-}
-
 void ATank::SetBarrelReference(UTankBarrel* BarrelToSet)
 {
     TankAimingComponent->SetBarrelReference(BarrelToSet);
@@ -42,5 +37,15 @@ void ATank::SetBarrelReference(UTankBarrel* BarrelToSet)
 void ATank::SetTurretReference(UTankTurret* TurretToSet)
 {
     TankAimingComponent->SetTurretReference(TurretToSet);
+}
+
+void ATank::AimAt(FVector HitLocation)
+{
+    TankAimingComponent->AimAt(HitLocation, LaunchSpeed);
+}
+
+void ATank::Fire()
+{
+    UE_LOG(LogTemp, Warning, TEXT("Tank fires"));
 }
 
